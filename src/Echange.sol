00f48457dc35e3abb5426e17a2a60736d2777bae
abcode <mabsattar@proton.me>
2024-08-30 03:08:42 +0530

    created view function, added functionality to liquidity & created an quivalent removeLiquidiity function

--- src/Echange.sol
+++ src/Echange.sol
@@ -4,4 +4,88 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Exchange is ERC20 {}
+contract Exchange is ERC20 {
+    address public tokenAddress;
+
+    constructor(address token) ERC20("ETH TOKEN LP Token", "lpETHTOKEN") {
+        require(token != address(0), "Token address passed is a null address");
+        tokenAddress = token;
+    }
+
+    function getReserve() public view returns (uint256) {
+        return ERC20(tokenAddress).balanceOf(address(this));
+    }
+
+    // addLiquidity allows users to add liquidity to the exchange
+    function addLiquidity(
+        uint256 amountOfToken
+    ) public payable returns (uint256) {
+        uint256 lpTokensToMint;
+        uint256 ethReserveBalance = address(this).balance;
+        uint256 tokenReserveBalance = getReserve();
+
+        ERC20 token = ERC20(tokenAddress);
+
+        // If the reserve is empty, take any user supplied value for initial liquidity
+        if (tokenReserveBalance == 0) {
+            // Transfer the token from the user to the exchange
+            token.transferFrom(msg.sender, address(this), amountOfToken);
+
+            // lpTokensToMint = ethReserveBalance = msg.value
+            lpTokensToMint = ethReserveBalance;
+
+            // Mint LP tokens to the user
+            _mint(msg.sender, lpTokensToMint);
+
+            return lpTokensToMint;
+        }
+
+        // If the reserve is not empty, calculate the amount of LP Tokens to be minted
+        uint256 ethReservePriorToFunctionCall = ethReserveBalance - msg.value;
+        uint256 minTokenAmountRequired = (msg.value * tokenReserveBalance) /
+            ethReservePriorToFunctionCall;
+
+        require(
+            amountOfToken >= minTokenAmountRequired,
+            "Insufficient amount of tokens provided"
+        );
+
+        // Transfer the token from the user to the exchange
+        token.transferFrom(msg.sender, address(this), minTokenAmountRequired);
+
+        // Calculate the amount of LP tokens to be minted
+        lpTokensToMint =
+            (totalSupply() * msg.value) /
+            ethReservePriorToFunctionCall;
+
+        // Mint LP tokens to the user
+        _mint(msg.sender, lpTokensToMint);
+
+        return lpTokensToMint;
+    }
+
+    function removeLiquidity(
+        uint256 amountOfLPTokens
+    ) public returns (uint256, uint256) {
+        require(
+            amountOfLPTokens > 0,
+            "Amount of tokens to remove must be greater than 0"
+        );
+
+        uint256 ethReserveBalance = address(this).balance;
+        uint256 lpTokenTotalSupply = totalSupply();
+
+        // Calculate the amount of ETH and tokens to return to the user
+        uint256 ethToReturn = (ethReserveBalance * amountOfLPTokens) /
+            lpTokenTotalSupply;
+        uint256 tokenToReturn = (getReserve() * amountOfLPTokens) /
+            lpTokenTotalSupply;
+
+        // Burn the LP tokens from the user, and transfer the ETH and tokens to the user
+        _burn(msg.sender, amountOfLPTokens);
+        payable(msg.sender).transfer(ethToReturn);
+        ERC20(tokenAddress).transfer(msg.sender, tokenToReturn);
+
+        return (ethToReturn, tokenToReturn);
+    }
+}
